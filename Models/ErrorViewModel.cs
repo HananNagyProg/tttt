51e763ac4f23dedaa9d9492a2f974993cfb20d2e
Hanan Nagy <hanan_en@outlook.sa>
2023-09-13 09:16:31 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tttt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
